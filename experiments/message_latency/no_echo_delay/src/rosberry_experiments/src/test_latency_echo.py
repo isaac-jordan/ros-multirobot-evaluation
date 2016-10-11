b5ebf2cf3b0f531b19e133e6699d8b682062cb7b
Isaac Jordan <isaacjordan95@gmail.com>
2016-10-11 16:37:50 +0100

    Automated frequency-sweep, and program will now compile all runs into one handy-dandy CSV file.

--- experiments/message_latency/no_echo_delay/src/rosberry_experiments/src/test_latency_echo.py
+++ experiments/message_latency/no_echo_delay/src/rosberry_experiments/src/test_latency_echo.py
@@ -5,14 +5,15 @@ from rosberry_experiments.msg import StampedMessage
 import time
 import sys
 
+N_MESSAGES = 1000
+
 def listener(msg, args):
     pub = args[0]
     pub.publish(msg)
 
 def main():
-    N = int(sys.argv[2])
     rospy.init_node('talker1', anonymous=True)
-    pub = rospy.Publisher('chatter_s', StampedMessage, queue_size=N)
+    pub = rospy.Publisher('chatter_s', StampedMessage, queue_size=N_MESSAGES)
     sub = rospy.Subscriber("chatter_m", StampedMessage, listener, callback_args=[pub])
     try:
         rospy.spin()
--- experiments/message_latency/no_echo_delay/src/rosberry_experiments/src/test_latency_main.py
+++ experiments/message_latency/no_echo_delay/src/rosberry_experiments/src/test_latency_main.py
@@ -2,44 +2,85 @@
 
 import rospy
 from rosberry_experiments.msg import StampedMessage
-import time
-import sys
+import sys, csv, itertools, os
 
-N = None
-RATE = None
-f = None
+MIN_FREQ = 500
+MAX_FREQ = 10000
+FREQ_STEP = 500
+N_MESSAGES = 1000
+
+# Stores current run's data
+data = {
+    "sent_time" : [None] * N_MESSAGES,
+    "recv_time" : [None] * N_MESSAGES
+}
 
 def listener(msg):
     recv_time = rospy.get_rostime()
     sent_time = msg.t
-    f.write(str(msg.id) + "," + str(sent_time) + "," + str(recv_time) + "\n")
 
-def talker():
-    pub = rospy.Publisher('chatter_m', StampedMessage, queue_size=N)
+    data["sent_time"][msg.id] = sent_time
+    data["recv_time"][msg.id] = recv_time
+
+def talker(numberOfMessages, frequency):
+    pub = rospy.Publisher('chatter_m', StampedMessage, queue_size=numberOfMessages)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(RATE)
-    for i in xrange(N):
+    rate = rospy.Rate(frequency)
+    for i in xrange(numberOfMessages):
         hello_str = "hello world"
         timestamp = rospy.get_rostime()
-        pub.publish(id=i, t=timestamp, message=hello_str )
-
+        pub.publish(id=i, t=timestamp, message=hello_str)
         rate.sleep()
 
 def main():
-    global RATE, N, f
-    RATE = int(sys.argv[1])
-    N = int(sys.argv[2])
-    f = open("times_"+str(RATE)+".txt", "w+")
-    print RATE, N
-    try:
-        sub = rospy.Subscriber("chatter_s", StampedMessage, listener)
-        talker()
-        rospy.sleep(5)
-        f.close()
+    if os.path.isfile("results.csv"):
+        os.remove("results.csv")
 
-    except rospy.ROSInterruptException:
-        print "Exception: ROSInterruptException"
+    currentFrequency = MIN_FREQ
+    while (currentFrequency <= MAX_FREQ):
+        print str(N_MESSAGES) + " msgs at " + str(currentFrequency) + " Hz."
+        try:
+            sub = rospy.Subscriber("chatter_s", StampedMessage, listener)
+            talker(N_MESSAGES, currentFrequency)
+
+            # Sleep to allow for slow messages to catch up
+            rospy.sleep(10)
+        except rospy.ROSInterruptException:
+            print "Exception: ROSInterruptException"
+
+        # Write out data
+        writeToCsv(currentFrequency)
+
+        # Reset data storage
+        data = {
+            "sent_time" : [None] * N_MESSAGES,
+            "recv_time" : [None] * N_MESSAGES
+        }
+        currentFrequency += FREQ_STEP
+
+        # Sleep to allow any file processing to finish
+        rospy.sleep(5)
 
 
 if __name__ == '__main__':
     main()
+
+# Utility
+def writeToCsv(currentFrequency):
+    if os.path.isfile("results.csv"):
+        with open("results.csv", "rb") as csv_in:
+            csvReader = csv.reader(csv_in)
+            with open("results_temp.csv", "wb") as csv_out:
+                csvWriter = csv.writer(csv_out)
+                csvWriter.writerow(next(csvReader) + ['sent_time' + str(currentFrequency), "recv_time" + str(currentFrequency)])
+                for msg_id, sent_time, recv_time in itertools.izip_longest(xrange(len(data["sent_time"])), data["sent_time"], data["recv_time"]):
+                    csvWriter.writerow(next(csvReader) + [sent_time, recv_time])
+                csv_out.flush()
+        os.rename("results_temp.csv", "results.csv")
+    else:
+        with open("results.csv", "wb") as csv_out:
+            csvWriter = csv.writer(csv_out)
+            csvWriter.writerow(["id", "sent_time" + str(currentFrequency), "recv_time" + str(currentFrequency)])
+            for msg_id, sent_time, recv_time in itertools.izip_longest(xrange(len(data["sent_time"])), data["sent_time"], data["recv_time"]):
+                csvWriter.writerow([msg_id, sent_time, recv_time])
+            csv_out.flush()
