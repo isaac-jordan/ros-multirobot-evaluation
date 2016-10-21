083c715a6da0f32c96f5990916ebcaf8f06d805f
Natalia Chechina <Natalia.Chechina@glasgow.ac.uk>
2016-10-21 17:47:48 +0100

    Add plots from latest set of experiments

--- discussion/gnuplot/run_all.sh
+++ discussion/gnuplot/run_all.sh
@@ -22,6 +22,28 @@ command="gnuplot plotter_exp1_msg_latency_two_1000khz.txt"
 echo ">>"$command"<<"
 $command
 
+command="gnuplot plotter_exp1_msg_latency_restart_10khz.txt"
+echo ">>"$command"<<"
+$command
+
+command="gnuplot plotter_exp1_msg_latency_restart_100khz.txt"
+echo ">>"$command"<<"
+$command
+
+command="gnuplot plotter_exp1_msg_latency_restart_1000khz.txt"
+echo ">>"$command"<<"
+$command
+
+command="gnuplot plotter_exp1_msg_latency_full_restart.txt"
+echo ">>"$command"<<"
+$command
+
+command="gnuplot plotter_exp1_msg_latency_partial_restart.txt"
+echo ">>"$command"<<"
+$command
+
+
+
 for f in *.eps; do
 	epstopdf $f
 	rm $f
